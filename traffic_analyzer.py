9647320aba627be1d563d2d087fe4b3390046277
Leo <115377071+Barrosleo@users.noreply.github.com>
2024-09-16 14:39:51 +0100

    Add network traffic analyzer script

---
+++ traffic_analyzer.py
@@ -0,0 +1,26 @@
+import socket
+import struct
+from datetime import datetime
+
+# Function to Capture Packets
+def capture_packets():
+    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+    while True:
+        raw_data, addr = conn.recvfrom(65536)
+        dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
+        print('\nEthernet Frame:')
+        print(f'Destination: {dest_mac}, Source: {src_mac}, Protocol: {eth_proto}')
+        # Add more detailed analysis here
+
+# Function to Unpack Ethernet Frame
+def ethernet_frame(data):
+    dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
+    return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[14:]
+
+def get_mac_addr(bytes_addr):
+    bytes_str = map('{:02x}'.format, bytes_addr)
+    return ':'.join(bytes_str).upper()
+
+# Function to Run the Analyzer
+if __name__ == "__main__":
+    capture_packets()
